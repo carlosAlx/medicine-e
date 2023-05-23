efe79a0cc47f5da4d6d4b22bab612a8b40c6a8b0
carlosAlx <53825590+carlosAlx@users.noreply.github.com>
2023-05-23 11:05:06 -0300

    iniciando page Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Router, Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
 import { Header } from "./components/header/Header";
 import { Switch } from "react-router-dom/cjs/react-router-dom";
@@ -7,12 +7,12 @@ import { Home } from "./pages/Home";
 function App() {
   return (
     <>
-      <Router>
+      <BrowserRouter>
         <Header />
         <Switch>
           <Route exact path="/" component={Home} />
         </Switch>
-      </Router>
+      </BrowserRouter>
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
 export const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  window.addEventListener("scroll", function () {
+    const header = this.document.querySelector(".header");
+    header.classList.toggle("active", this.window.scrollY > 100);
+  });
+  window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
+  return <div>Home</div>;
+};
